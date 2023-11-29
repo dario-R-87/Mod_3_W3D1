cd0718b97ee5001f0902d09ab82f9c98bd9faf36
Dario Russo <dariorusso987@gmail.com>
2023-11-29 12:17:01 +0100

    up

--- index.js
+++ index.js
@@ -42,16 +42,76 @@ const courses = [
         nome: "Nome 4",
         cognome: "Cognome 4",
         img_prof: "avatar-4"
+    },
+    {
+        url_img: "course-html",
+        titolo: "Titolo 5",
+        durata: 120,
+        livello: "Principiante",
+        media_voti: 3.5,
+        prezzo: 19.99,
+        nome: "Nome 5",
+        cognome: "Cognome 5",
+        img_prof: "avatar-5"
+    },
+    {
+        url_img: "course-javascript",
+        titolo: "Titolo 6",
+        durata: 90,
+        livello: "Avanzato",
+        media_voti: 4.8,
+        prezzo: 34.99,
+        nome: "Nome 6",
+        cognome: "Cognome 6",
+        img_prof: "avatar-6"
+    },
+    {
+        url_img: "course-laravel",
+        titolo: "Titolo 7",
+        durata: 150,
+        livello: "Principiante",
+        media_voti: 3.9,
+        prezzo: 22.99,
+        nome: "Nome 7",
+        cognome: "Cognome 7",
+        img_prof: "avatar-7"
+    },
+    {
+        url_img: "course-node",
+        titolo: "Titolo 8",
+        durata: 180,
+        livello: "Avanzato",
+        media_voti: 4.6,
+        prezzo: 39.99,
+        nome: "Nome 8",
+        cognome: "Cognome 8",
+        img_prof: "avatar-8"
+    },
+    {
+        url_img: "course-python",
+        titolo: "Titolo 9",
+        durata: 120,
+        livello: "Intermedio",
+        media_voti: 4.3,
+        prezzo: 27.99,
+        nome: "Nome 9",
+        cognome: "Cognome 9",
+        img_prof: "avatar-2"
     }
 ];
 
 const carosel = document.querySelector("#carouselExample .carousel-inner");
-carosel.innerHTML += `<div class="carousel-item active d-flex gap-3 justify-content-center"></div>`;
-const carosel__item = document.querySelector("#carouselExample .carousel-inner .carousel-item");
 
-for(let i=0; i<courses.length; i++){
+const end_for = courses.length%4 === 0 ? Math.round(courses.length/4) : Math.round(courses.length/4)+1;
+
+for(let x=0; x<end_for; x++){    
+carosel.innerHTML += `<div id="item_${x}" class="carousel-item ${x===0 ? 'active' : ''} d-flex gap-3 justify-content-between"></div>`;
+const carosel__item = document.querySelector(`#carouselExample .carousel-inner #item_${x}`);
+
+const offset = (x===end_for-1 ? courses.length%4 : 4);
+for(let i=x*4; i<x*4+offset; i++){
  carosel__item.innerHTML += `
- <div class="col-3">
+ <div class="col-6 col-md-4 col-lg-3">
   <div class="card" >
    <img src="./assets/${courses[i].url_img}.jpg" class="card-img-top" alt="...">
    <div class="card-body">
@@ -87,3 +147,11 @@ ${courses[i].prezzo} ${courses[i].prezzo+200}
 
 </div>`;
 }
+}
+
+const ciao = function(){
+
+};
+
+const user = document.querySelector("#btn_user");
+user.onclick = ciao;
